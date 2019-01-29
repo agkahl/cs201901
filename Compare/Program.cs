da097b7eb4e95f9ebb4ea422e0a004cfde34ec71
Anne Grethe Kahl <aka@prelectronics.com>
2019-01-29 11:32:16 +0100

    dag6_del1

---
+++ Compare/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hund[] hunde = new Hund[6];
+            hunde[0] = new Hund() { Alder = 10, Navn = "Bulder" };
+            hunde[1] = new Hund() { Alder = 5, Navn = "Lady" };
+            hunde[2] = new Hund() { Alder = 1, Navn = "Rosa" };
+            hunde[3] = new Hund() { Alder = 4, Navn = "Luna" };
+            hunde[4] = new Hund() { Alder = 6, Navn = "Asta" };
+            hunde[5] = new Hund() { Alder = 12, Navn = "Bine" };
+            Array.Sort(hunde);
+            foreach (var item in hunde)
+            {
+                Console.WriteLine(item.Navn);
+            }
+
+            // Hold console åben ved debug
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+        }
+    }
+    class Hund : IComparable<Hund>
+    {
+        public string Navn { get; set; }
+        public int Alder { get; set; }
+
+        public int CompareTo(Hund other)
+        {
+            if (other.Alder > this.Alder)
+                return 1;
+            if (other.Alder < this.Alder)
+                return -1;
+            return 0;
+        }
+    }
+}
---
+++ Interface_Eget/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_Eget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Anvendelse af array
+            IDbFunktioner[] array = new IDbFunktioner[4];
+            array[0] = new Hund();
+            array[1] = new Ubåd();
+            array[2] = new Hund();
+            array[3] = new Ubåd();
+
+            foreach (var item in array)
+            {
+                item.Gem();
+            }
+
+            //Anvendelse af list
+            List<IDbFunktioner> lst1 = new List<IDbFunktioner>();
+            lst1.Add(new Hund());
+            lst1.Add(new Ubåd());
+            lst1.Add(new Hund());
+            lst1.Add(new Ubåd());
+
+            foreach (var item in lst1)
+            {
+                item.Gem();
+            }
+
+            // Hold console åben ved debug
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+        }
+    }
+    
+    interface IDbFunktioner
+    {
+        void Gem();
+    }
+
+    class Hund : IDbFunktioner
+    {
+        public string Navn { get; set; }
+        void IDbFunktioner.Gem()
+        {
+            Console.WriteLine("Gemmer hund...");
+        }
+    }
+
+    class Ubåd : IDbFunktioner
+    {
+        public int Nummer { get; set; }
+        public double Tubine { get; set; }
+        public void Gem()
+        {
+            Console.WriteLine("Gemmer ubåd...");
+        }
+    }
+}
